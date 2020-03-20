29143613767d7623d9939ae04f5a3565f7f936ef
Daniel <Daniel.kotrba@gmx.at>
2020-03-20 14:30:13 +0100

    bubble sort update with methods

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
         int[] numbers = {86, 78, -48, 4, 9 - 8};
         System.out.println(Arrays.toString(numbers));
         array(numbers);
     }
-
     private static void array(int[] numbers) {
         int temp = 0;
         for (int i = 0; i < numbers.length - 1; i++) {
